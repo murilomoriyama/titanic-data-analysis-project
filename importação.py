c03127a1815652ba4ed832751c25a1c0a867eac0
ArthurRamos159 <arthurodrigues804@hotmail.com>
2026-06-23 21:52:32 -0300

    importação.py

---
+++ importação.py
@@ -0,0 +1,11 @@
+import os
+import csv
+
+matriz = []
+
+if os.path.isfile("train.csv") == False:
+  print("Arquivo não encontrado.")
+else:
+  with open("train.csv", "r") as f:
+    conteudo = 
+  
