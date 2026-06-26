c9d162de7f1fcf54b57fb92f43a607bbf60a4e17
Yan Sevy Garcia Belanger <yansevy@alunos.utfpr.edu.br>
2026-06-25 21:10:55 -0300

    Update visualização.py
    Funcoes

--- visualização.py
+++ visualização.py
@@ -4,3 +4,9 @@ Exibir o cabeçalho com os nomes das colunas (ex: Name, Age, Survived).
 Permitir visualizar os primeiros N passageiros (ex: as 5 primeiras linhas).
 Listar todos os dados de forma organizada.
 '''
+
+from importação import carregar_dados
+
+def visualizar_cabecalho()
+
+def visualizar_dados()
