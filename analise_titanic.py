4092dc78c1d871704eb2d9c733e44d354d7de519
ArthurRamos159 <arthurodrigues804@hotmail.com>
2026-06-25 21:36:48 -0300

    Update analise_titanic.py

--- analise_titanic.py
+++ analise_titanic.py
@@ -1,5 +1,7 @@
 from importação import carregar_dados
 
-carregar_dados()
+arquivo = "train.csv"
+
+coluna, linha = carregar_dados()
 def contagem_geral():
-  print(len(matriz_dados[0]))
+  print(len(linha))
