08b3593d0176d40687dbb8be3d9370f4aaeabed0
Yan Sevy Garcia Belanger <yansevy@alunos.utfpr.edu.br>
2026-06-25 21:07:12 -0300

    Update visualização.py
    Comentarios para lembrar os processos

---
+++ visualização.py
@@ -0,0 +1,6 @@
+# Yan - Puxar de importação para fazer a analise
+'''
+Exibir o cabeçalho com os nomes das colunas (ex: Name, Age, Survived).
+Permitir visualizar os primeiros N passageiros (ex: as 5 primeiras linhas).
+Listar todos os dados de forma organizada.
+'''
