92aba45774df22351be9c7bf21c9a454a1bada8c
Yan Sevy Garcia Belanger <yansevy@alunos.utfpr.edu.br>
2026-06-28 23:49:32 -0300

    Update: Add docstring das funções desenvolvidas -> visualização.py

--- visualização.py
+++ visualização.py
@@ -1,17 +1,42 @@
-'''
-Exibir o cabeçalho com os nomes das colunas (ex: Name, Age, Survived).
-Permitir visualizar os primeiros N passageiros (ex: as 5 primeiras linhas).
-Listar todos os dados de forma organizada. FEITO
-'''
+"""
+Módulo responsável pela visualização dos dados do conjunto Titanic.
+Contém funções para exibir o cabeçalho e visualizar parte do conteúdo
+da base de dados.
+"""
 
 def visualizar_cabecalho(cabecalho):
+    """
+    Exibe o cabeçalho da tabela de dados.
+
+    Parâmetros:
+    - cabecalho (list): Lista contendo os nomes das colunas do arquivo CSV.
+
+    Retorno:
+    - None: Exibe o cabeçalho na tela.
+    """
+
     print(cabecalho)
 
+
 def visualizar_conteudo(dados):
+    """
+    Exibe os primeiros registros da base de dados.
+
+    Solicita ao usuário a quantidade de passageiros que deseja visualizar.
+    Caso o valor informado seja maior que a quantidade de registros,
+    exibe todos os passageiros disponíveis.
+
+    Parâmetros:
+    - dados (list): Lista de listas contendo os dados dos passageiros.
+
+    Retorno:
+    - None: Exibe os registros solicitados na tela.
+    """
+
     N = int(input("Insira quantos passageiros quer ver: "))
 
     if N > len(dados):
         N = len(dados)
 
-    for i in range (N):
+    for i in range(N):
         print(dados[i])
